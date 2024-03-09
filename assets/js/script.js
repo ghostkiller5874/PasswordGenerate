636d31686b64063c300c62e23093f3908c19de4a
Eder Moreira <feelsgoback967@outlook.com>
2024-03-09 12:45:52 -0300

    captura das opções selecionadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,9 @@
-//caso seleciona as criptografias, criar um input com o resultado
 const state = {
     content: {
-        conteudo: document.querySelectorAll(".contentPass .content-check"),
-        tamanho: document.querySelectorAll(".SizePass .size-check"),
-        cripto: document.querySelectorAll(".cript .cript-check"),
         password: document.querySelector(".gerador #senha")
     },
-    value:{
-        buttonGenerate: document.querySelector("#gerarPass")
+    actions: {
+        buttonGenerate: document.getElementById("gerarPass")
     },
 }
 const especiais = [
@@ -67,6 +63,11 @@ const letras = [
     "z"
 ];
 
+/**
+ * 
+ * Gera valores de forma aleatoria
+ * 
+ */
 function randomNumeral() {
     let numeroRandom = Math.floor(Math.random() * 10).toString()
     return numeroRandom;
@@ -76,38 +77,103 @@ function randomLetra() {
     let letraRandom = Math.floor(Math.random() * letras.length).toString()
     return letras[letraRandom]
 }
+function randomLetraUpper() {
+    return randomLetra().toUpperCase()
+}
 
 function randomChar() {
     let charRandom = Math.floor(Math.random() * especiais.length).toString()
     return especiais[charRandom]
 }
 
+/**
+ * 
+ * Checa se o tamanho foi acionado(marcado como checked) 
+ * 
+ * Checa o conteudo add (que foi checked)
+ * 
+ */
 
-function gerar(numeros, letras, simbolos) {
-    let password = "";
-    let tamanho = 10;
+function tamanhoCheck() {
+    let T8 = document.getElementById(`8`)
+    let T16 = document.getElementById(`16`)
+    let T25 = document.getElementById(`25`)
+    let T32 = document.getElementById(`32`)
+    let T64 = document.getElementById(`64`)
 
-    const conjunto = [numeros,letras,simbolos];
-
-
-    for (let i = 0; i < tamanho; i=i+ conjunto.length) {
-        conjunto.forEach(()=>{
-            const randomValue = conjunto[Math.floor(Math.random() * conjunto.length)]();
-            //coloca o parenteses no final por ser uma lista de funçoes
-             password+=randomValue
-        });
-       
+    if (T8.checked) {
+        if (T8.value === T8.id) return T8.id
     }
-   return password 
+    if (T16.checked) {
+        if (T16.value === T16.id) return T16.id
+    }
+    if (T25.checked) {
+        if (T25.value === T25.id) return T25.id
+    }
+    if (T32.checked) {
+        if (T32.value === T32.id) return T32.id
+    }
+    if (T64.checked) {
+        if (T64.value === T64.id) return T64.id
+    }
+}
+function conteudoCheck() {
+    
+    let numero = document.getElementById("numeros")
+    let maiuscula = document.getElementById("maiuscula")
+    let minuscula = document.getElementById("minuscula")
+    let outros = document.getElementById("outros")
+    let conteudo = [];
+    
+    if(numero.checked){
+        if(numero.value === numero.id) conteudo.push(randomNumeral()) 
+    }
+    if(maiuscula.checked){
+        if(maiuscula.value === maiuscula.id) conteudo.push(randomLetraUpper())
+    }
+    if(minuscula.checked){
+        if(minuscula.value === minuscula.id) conteudo.push(randomLetra())
+    }
+    if(outros.checked){
+        if(outros.value === outros.id) conteudo.push(randomChar())
+    }
+
+    return conteudo
+}
+
+
+
+/**
+ * 
+ * Função que gera o senha
+ * 
+ */
+function gerar() {
+    let password = "";
+    let tamanho = tamanhoCheck();
+
+    //const conjunto = conteudo;
+    const conjunto = [conteudoCheck()];
+
+
+    for (let i = 0; i < tamanho; i = i + conjunto.length) {
+        conjunto.forEach(() => {
+            const randomValue = conjunto[Math.floor(Math.random() * conjunto.length)];
+            //coloca o parenteses no final por ser uma lista de funçoes
+            password += randomValue
+        });
+
+    }
+    //return password.toString().split(",")
+    return password
 }
 
 
 function init() {
-    state.value.buttonGenerate.addEventListener("click",()=>{
-        state.content.password.value = gerar(randomNumeral,randomLetra,randomChar);
-       
-       //console.log(randomChar)
-    })
     
+    state.actions.buttonGenerate.addEventListener("click",()=>{
+        state.content.password.value = gerar(conteudoCheck);
+    })
+    //conteudoCheck()
 }
 init();
